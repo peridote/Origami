da3a6e769c2706151bba0a7f57d49f418a08747a
peridote <3rlatpghks3@naver.com>
2016-06-10 22:47:57 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.sehwan.origami;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends ListActivity {
+
+    ArrayList<String> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new OriSurfaceView(this));
+        //setContentView(new OriSurfaceView(this));
+        list = new ArrayList<String>();
+        list.add("Airplane");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
+        setListAdapter(adapter);
+
 
     }
 
@@ -20,5 +33,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String name = "Let's do origmai - "+list.get(position);
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getApplication(), OriSampleView.class);
+        startActivity(intent);
     }
 }
